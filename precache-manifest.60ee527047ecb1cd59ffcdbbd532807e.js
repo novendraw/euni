2639f1d9613d9f6c154691368436f2f6d7d83967
Eka Novendra Wahyunadi <ekanovendra726@gmail.com>
2019-05-25 13:04:55 +0700

    Add Static Page

---
+++ precache-manifest.60ee527047ecb1cd59ffcdbbd532807e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a808df304435314521d2d15a1cc6fb",
+    "url": "/euni/index.html"
+  },
+  {
+    "revision": "0f5325b3acabfa162892",
+    "url": "/euni/static/css/main.64890cc2.chunk.css"
+  },
+  {
+    "revision": "48b68d6fafe59d7c93a3",
+    "url": "/euni/static/js/2.b5adb1bc.chunk.js"
+  },
+  {
+    "revision": "0f5325b3acabfa162892",
+    "url": "/euni/static/js/main.612208aa.chunk.js"
+  },
+  {
+    "revision": "d74128dc198f7683bfd1",
+    "url": "/euni/static/js/runtime~main.ef66bfa3.js"
+  }
+]);
